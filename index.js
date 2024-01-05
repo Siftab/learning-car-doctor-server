ef4397fbe9dba9e5d01adbdc5782d33ab1662ab5
Siftab <siiffuuu@gmail.com>
2024-01-05 20:30:34 +0600

    started

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -27,6 +27,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const carServices=client.db("car-doctor").collection("services");
+// get all service data 
+
+    app.get('/services',async(req,res)=>{
+      const result =await carServices.find().toArray();
+      res.send(result)
+    })
+// get single services data
+app.get('/services/:id',async(req,res)=>{
+  const id = req.params.id;
+  const filter= {
+    _id: new ObjectId(id)
+  }
+
+  const options = {
+    
+    // Include only the `title` and `imdb` fields in the returned document
+    projection: {  title: 1, price:1,description:1 },
+  };
+  const result=await carServices.findOne(filter,options)
+  res.send(result)
+})
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
